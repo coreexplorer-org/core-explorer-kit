db9ca124c3404fcc2b88843527c66093e2e582f4
Carter Cole <carter.cole@gmail.com>
2025-05-09 09:12:11 -0500

    got backend up

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,22 +1,22 @@
 import socket
-from .schema import schema
+from schema import schema
 from flask_graphql import GraphQLView
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/api/")
 def hello():
     html = "<h3>Hello Fullstack!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" 
 
     return html.format(hostname=socket.gethostname())
 
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql',
+app.add_url_rule('/api/graphql', view_func=GraphQLView.as_view('graphql',
                                                                  schema=schema,
                                                                  graphiql=True))
 
 if __name__ == "__main__":
 
-    app.run(host='0.0.0.0')
+    app.run(host='::', debug=True)
