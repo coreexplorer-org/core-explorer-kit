f0e6fc399482e30cc52df36efbc39f8418c896b6
itsrealfake <107151866+itsrealfake@users.noreply.github.com>
2025-05-09 14:56:31 -0500

    stretch

--- backend/app/schema.py
+++ backend/app/schema.py
@@ -43,6 +43,23 @@ class Actor(graphene.ObjectType):
     authored_commits = graphene.List(Commit)
     committed_commits = graphene.List(Commit)
 
+class FileDetailRecord(graphene.ObjectType):
+    # match (f :FileDetailRecord) return f
+    #     {
+    #   "identity": 46253,
+    #   "labels": [
+    #     "FileDetailRecord"
+    #   ],
+    #   "properties": {
+    #     "file_path": "src/policy",
+    #     "master_sha_at_collection": "9a05b45da60d214cb1e5a50c3d2293b1defc9bb0",
+    #     "json_blob": "{"master_sha_at_collection": "9a05b45da60d214cb1e5a50c3d2293b1defc9bb0", "file_paths": "src/policy", "length_of_unique_authors": 67, "unique_author_names": ["Matthew Zipkin", "Peter Todd", "Kristaps Kaupe", "Wladimir J. van der Laan", "Gregory Maxwell", "CryptAxe", "MeshCollider", "Murch", "Akira Takizawa", "esneider", "Gloria Zhao", "Chris Wheeler", "Greg Sanders", "glozow", "TheCharlatan", "Matthew English", "Matt Whitlock", "Mark Friedenbach", "Gregory Sanders", "Pieter Wuille", "James O'Beirne", "Jonas Schnelli", "Dan Raviv", "Vasil Dimov", "Sebastian Falbesoner", "Jon Atack", "DrahtBot", "W. J. van der Laan", "Ava Chow", "Kiminuo", "Matt Corallo", "BtcDrak", "Sjors Provoost", "Ben Woosley", "MacroFake", "Samuel Dobson", "fanquake", "Jim Posen", "dergoegge", "ismaelsadeeq", "Luke Dashjr", "Anthony Towns", "practicalswift", "Philip Kaufmann", "Ryan Ofsky", "sanket1729", "CAnon", "Marcel Kr\u00fcger", "MarcoFalke", "Carl Dong", "John Newbery", "Johnson Lau", "Brian Deery", "Hennadii Stepanov", "Russell Yanofsky", "Veres Lajos", "Karl-Johan Alm", "Jorge Tim\u00f3n", "stickies-v", "Alex Morcos", "Suhas Daftuar", "Andrew Chow", "isle2983", "Dimitris Tsapakidis", "Antoine Poinsot", "Pavel Jan\u00edk", "marcofleon"], "length_of_all_commits": 467}"
+    #   },
+    #   "elementId": "4:9870c522-5c84-4950-aa9b-9e8143f7c69c:46253"
+    # }
+    file_path = graphene.String()
+    master_sha_at_collection = graphene.String()
+    json_blob = graphene.String()
 
 class Query(graphene.ObjectType):
     hello = graphene.String(description="A typical hello world")
@@ -60,26 +77,26 @@ class Query(graphene.ObjectType):
 
     actor = graphene.Field(Actor, email=graphene.String(required=True))
 
-    def resolve_actor(self, info, email):
+    file_detail_records = graphene.List(FileDetailRecord, description="file_detail_record")
+
+    def file_detail_records(self, info):
         db = Neo4jDriver()
-        data = db.get_actor_with_commits(email)
+        file_detail_records = db.get_all_file_detail_records()
+        db.close()
+        return [FileDetailRecord(**file_detail_record) for file_detail_record in file_detail_records]
+
+    file_detail_record = graphene.Field(FileDetailRecord, file_path=graphene.String(required=True))
+
+    def resolve_file_detail_record(self, info, email):
+        db = Neo4jDriver()
+        data = db.get_file_detail_record_with_commits(email)
         if not data:
             return None
-        return Actor(
-            name=data["name"],
-            email=data["email"],
-            authored_commits=[
-                Commit(
-                    commit_hash=commit.get("commit_hash"),
-                    message=commit.get("message")
-                ) for commit in data["authored_commits"]
-            ],
-            committed_commits=[
-                Commit(
-                    commit_hash=commit.get("commit_hash"),
-                    message=commit.get("message")
-                ) for commit in data["committed_commits"]
-            ]
+
+        return FileDetailRecord(
+            file_path=data["file_path"],
+            master_sha_at_collection=data["master_sha_at_collection"],
+            json_blob=data['json_blob']
         )
     repository = graphene.Field(Repository, name=graphene.String(required=True))
 
