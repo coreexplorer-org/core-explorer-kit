3d7a6b85ee3e6b81639f122b626394912862fb65
Carter Cole <carter.cole@gmail.com>
2025-05-08 21:12:41 -0500

    carters

---
+++ init/init.sh
@@ -0,0 +1,8 @@
+cd ..
+
+git clone https://github.com/coreexplorer-org/repo_explorer.git
+git clone https://github.com/coreexplorer-org/repex.git
+git clone https://github.com/coreexplorer-org/CE_demo.git
+
+
+doccker-compose up --build
